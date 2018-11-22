f0dd4ca7d209ceada28b0ef309ce73a92ba0249c
kelvin111027 <kaikang@umich.edu>
2018-11-22 17:44:34 -0500

    fix the keys in countries table

--- proj3_choc.py
+++ proj3_choc.py
@@ -11,18 +11,49 @@ import json
 DBNAME = 'choc.db'
 BARSCSV = 'flavors_of_cacao_cleaned.csv'
 COUNTRIESJSON = 'countries.json'
-def create_choc_db():
-    DBNAME = 'choc.db'
-    BARSCSV = 'flavors_of_cacao_cleaned.csv'
-    COUNTRIESJSON = 'countries.json'
+
+def create_populate_Countries():
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
+    statement = '''
+       DROP TABLE IF EXISTS 'Countries';
+    '''
+    cur.execute(statement)
+    conn.commit()
+
+    statement = '''
+        CREATE TABLE 'Countries'(
+            'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+            'Alpha2' TEXT,
+            'Alpha3' TEXT,
+            'EnglishName' TEXT,
+            'Region' TEXT,
+            'Subregion' TEXT,
+            'Population' INTEGER,
+            'Area' REAL
+        );
+    '''
+    cur.execute(statement)
+    conn.commit()
+
+    #open and read json file
+    with open(COUNTRIESJSON, encoding = 'utf-8') as countries_file:
+        countries_lst = json.load(countries_file)
+    countries_file.close()
+    for c in countries_lst:
+        insertion = (None, c['alpha2Code'], c['alpha3Code'], c['name'], c['region'], c['subregion'], c['population'], c['area'])
+        statement = 'INSERT INTO "Countries"'
+        statement += 'VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+        cur.execute(statement, insertion)
+    conn.commit()
+    conn.close()
+
+
+def create_populate_Bars():
     conn = sqlite3.connect(DBNAME)
     cur = conn.cursor()
     statement = '''
         DROP TABLE IF EXISTS 'Bars';
-    '''
-    cur.execute(statement)
-    statement = '''
-       DROP TABLE IF EXISTS 'Countries';
     '''
     cur.execute(statement)
     conn.commit()
@@ -41,50 +72,44 @@ def create_choc_db():
         );
     '''
     cur.execute(statement)
-    statement = '''
-        CREATE TABLE 'Countries'(
-            'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
-            'Alpha2' TEXT,
-            'Alpha3' TEXT,
-            'EnglishName' TEXT,
-            'Region' TEXT,
-            'Subregion' TEXT,
-            'Population' INTEGER,
-            'Area' REAL
-        );
-    '''
-    cur.execute(statement)
     conn.commit()
-    conn.close()
-
-def populate_choc_db():
-    conn = sqlite3.connect(DBNAME)
-    cur = conn.cursor()
     #open and read csv file
     with open("flavors_of_cacao_cleaned.csv") as f:
         csvReader = csv.reader(f)
         for row in csvReader:
             if row[0] != "Company" and row[2] != "REF" :
-                insertion = (None, row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8])
+                locationCountry = row[5]
+                stm_loc = 'SELECT Id FROM Countries WHERE EnglishName = ?'
+                cur.execute(stm_loc, (locationCountry,))
+                try:
+                    locationId = cur.fetchone()[0]
+                except:
+                    locationId = "NULL"
+
+                beanOriginCountry = row[8]
+                stm_ori = 'SELECT Id FROM Countries WHERE EnglishName = ?'
+                cur.execute(stm_ori, (beanOriginCountry,))
+                try:
+                    beanOriginId = cur.fetchone()[0]
+                #a format bug: the Côte d'Ivoire in csv file is not read correctly
+                except:
+                    if row[8] != "Unknown":
+                        cur.execute(stm_ori, ("Côte d'Ivoire",))
+                        beanOriginId = cur.fetchone()[0]
+                    else:
+                        beanOriginId = "NULL"
+
+                insertion = (None, row[0], row[1], row[2], row[3], row[4], locationId, row[6], row[7], beanOriginId)
                 statement = 'INSERT INTO "Bars"'
                 statement += 'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
                 cur.execute(statement, insertion)
         conn.commit()
-
-    #open and read json file
-    with open(COUNTRIESJSON, encoding = 'utf-8') as countries_file:
-        countries_lst = json.load(countries_file)
-    countries_file.close()
-    for c in countries_lst:
-        insertion = (None, c['alpha2Code'], c['alpha3Code'], c['name'], c['region'], c['subregion'], c['population'], c['area'])
-        statement = 'INSERT INTO "Countries"'
-        statement += 'VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
-        cur.execute(statement, insertion)
-    conn.commit()
     conn.close()
 
-create_choc_db()
-populate_choc_db()
+create_populate_Countries()
+create_populate_Bars()
+
+
 '''
 with open(COUNTRIESJSON, encoding = 'utf-8') as countries_file:
     countries_lst = json.load(countries_file)
